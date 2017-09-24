f93c413a73884dbaaf040a71cf098dc7c9bf5e78
lemonW <liangziwuli@qq.com>
2017-09-24 17:35:21 +0800

    完成index.html

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -2,17 +2,30 @@
  * @Author: huJiaFu 
  * @Date: 2017-09-18 16:49:46 
  * @Last Modified by: huJiaFu
- * @Last Modified time: 2017-09-20 16:28:23
+ * @Last Modified time: 2017-09-24 17:22:32
  */
-// require('./index.css')
+require('./index.css')
 //webpack配置文件中配置路径的别名的使用
 var _mm = require('util/mm.js');
+var templateBanner = require('./banner.string');
 
 require('page/common/nav-simple/index.js');
 require('page/common/nav/index.js');
-var navSide = require('page/common/nav-side/index.js');
 require('page/common/header/index.js');
+//引入jQuery轮播插件unslider
+require('util/slider/index.js')
 
-navSide.init({
-  name: 'user-center'
-});
+$(function () {
+  // 渲染banner的html
+  var bannerHtml = _mm.renderHtml(templateBanner);
+  $('.banner-con').html(bannerHtml);
+  // 初始化banner
+  var $slider = $('.banner').unslider({
+    dots: true
+  });
+  // 前一张和后一张操作的事件绑定
+  $('.banner-con .banner-arrow').click(function () {
+    var forward = $(this).hasClass('prev') ? 'prev' : 'next';
+    $slider.data('unslider')[forward]();
+  });
+})
